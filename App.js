16bba99e52f5f5576066d4bc8c16783d5b9f5ea5
Rakibul Alam <51063464+rakibulalam9200@users.noreply.github.com>
2022-03-08 17:57:17 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,97 +1,36 @@
 import * as React from 'react';
-import { View, Text,Button } from 'react-native';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
-const Tab = createBottomTabNavigator();
-function HomeScreen({navigation}) {
+function HomeScreen({ navigation }) {
   return (
-      // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      //   <Text>Home Screen</Text>
-      //   <Button
-      //       title="Go to Details"
-      //       onPress={() => navigation.navigate('Details')}
-      //   />
-          <Tab.Navigator screenOptions={{headerShown: false}}>
-              <Tab.Screen name="Feed" component={Feed} />
-              <Tab.Screen name="Messages" component={Messages} />
-          </Tab.Navigator>
-      // </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+            onPress={() => navigation.navigate('Notifications')}
+            title="Go to notifications"
+        />
+      </View>
   );
 }
 
-
-
-function Feed({navigation}){
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Feed Screen</Text>
-            <Button
-                title="Go to Details"
-                onPress={() => navigation.navigate('Details',{ screen: 'Work' })}
-            />
-        </View>
-    )
-}
-
-function Messages({navigation}){
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Messages Screen</Text>
-            <Button
-                title="Go to Details"
-                onPress={() => navigation.navigate('Details',{ screen: 'Work' })}
-            />
-        </View>
-    )
-}
-
-function SportDetails({navigation}){
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Sport Details Screen</Text>
-            <Button
-                title="Go to Home"
-                onPress={() => navigation.navigate('Home')}
-            />
-        </View>
-    )
-}
-
-function WorkDetails({navigation}){
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Work Details Screen</Text>
-            <Button
-                title="Go to Home"
-                onPress={() => navigation.navigate('Home')}
-            />
-        </View>
-    )
-}
-
-
-function DetailsScreen({navigation}) {
+function NotificationsScreen({ navigation }) {
   return (
-      <Tab.Navigator screenOptions={{headerShown: false}}>
-          <Tab.Screen name="Sport" component={SportDetails} />
-          <Tab.Screen name="Work" component={WorkDetails} />
-      </Tab.Navigator>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button onPress={() => navigation.goBack()} title="Go back home" />
+      </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        </Drawer.Navigator>
       </NavigationContainer>
   );
 }
